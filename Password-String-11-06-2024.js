79aa6e38bfc3d98754cc7071a6ad863dd8dfd943
devin_1 <22dcs001@charusat.edu.in>
2024-06-11 22:16:02 +0530

    naming convention changes

--- Password-String-11-06-2024.js
+++ Password-String-11-06-2024.js
@@ -1,17 +1,17 @@
 var fs = require('fs')
 let data;
-let new_data = [];
+let newData = [];
 let string = [];
-let user_id;
+let userID;
 
 fs.readFile('password.txt', function (err, item) {
     if (err) throw err;
     data = item.toString()
-    new_data = data.split('\n');
+    newData = data.split('\n');
 
-    new_data.map((item, index) => {
+    newData.map((item, index) => {
         string = item.split(":")
-        string[3] > 500 ? user_id = " , UserID :" + string[3] : user_id = "";
-        console.log("User : " + string[0] + " , " + "Command interpreter: " + string[6] + user_id)
+        string[3] > 500 ? userID = " , UserID :" + string[3] : userID = "";
+        console.log("User : " + string[0] + " , " + "Command interpreter: " + string[6] + userID)
     })
 })
