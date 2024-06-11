602cb826f86ac6dd4b988317882563c54b3207a4
devin_1 <22dcs001@charusat.edu.in>
2024-06-11 15:39:30 +0530

    improved code quality with map function

--- Password-String-11-06-2024.js
+++ Password-String-11-06-2024.js
@@ -8,13 +8,10 @@ fs.readFile('password.txt', function (err, item) {
     if (err) throw err;
     data = item.toString()
     new_data = data.split('\n');
-    for (let i = 0; i < new_data.length; i++) {
-        string[i] = new_data[i].split(':')
-    }    
-    for (let i = 0; i < string.length; i++) {
-        for (let j = 0; j < string[i].length; j++) {
-            string[i][3] > 500 ? user_id = " , UserID :" + string[i][3] : user_id = "";
-            console.log("User : " + string[i][0] + " , " + "Command interpreter: " + string[i][6] + user_id)
-        }
-    }
+
+    new_data.map((item, index) => {
+        string = item.split(":")
+        string[3] > 500 ? user_id = " , UserID :" + string[3] : user_id = "";
+        console.log("User : " + string[0] + " , " + "Command interpreter: " + string[6] + user_id)
+    })
 })
